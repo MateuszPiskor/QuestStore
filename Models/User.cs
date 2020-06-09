1fbfc3f9434ec4647f5759f49497b4b454a6a008
Mateusz Piskor <grytychy@gmail.com>
2020-06-09 22:45:05 +0200

    Add user model

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Queststore.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int MyProperty { get; set; }
+        public bool Is_Admin { get; set; }
+        public bool Is_Mentor { get; set; }
+        public int Student_id { get; set; }
+    }
+}
