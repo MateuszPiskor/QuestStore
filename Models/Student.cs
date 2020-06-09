71675d6c5ce0077fcb4c70b073f9926e412161f9
Mateusz Piskor <grytychy@gmail.com>
2020-06-09 22:50:25 +0200

    Add student user

---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Queststore.Models
+{
+    public class Student:User
+    {
+        new public int Id { get; set; }
+        public int Class_id { get; set; }
+        public int Team_id_coolcoins { get; set; }
+        public int Exp_level_id { get; set; }
+
+    }
+}
