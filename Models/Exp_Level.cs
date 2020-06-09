82028ddc55c3bc865a23512916be388f40311112
Mateusz Piskor <grytychy@gmail.com>
2020-06-09 23:12:30 +0200

    Add Expierience Level model

---
+++ Models/Exp_Level.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Queststore.Models
+{
+    public class Exp_Level
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Min_Points { get; set; }
+        public string Avatar { get; set; }
+
+    }
+}
