2f0986d0ff6cc83137884cbebb82143d8bcc8e15
Mateusz Piskor <grytychy@gmail.com>
2020-06-12 20:59:42 +0200

    Remove some properties in User class

--- Models/User.cs
+++ Models/User.cs
@@ -11,12 +11,30 @@ namespace Queststore.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public string Address { get; set; }
+        public string Adrress { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
-        public int MyProperty { get; set; }
-        public bool Is_Admin { get; set; }
-        public bool Is_Mentor { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsMentor { get; set; }
         public Student Student { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(int id, string name, string email, string phone,string adress, string login, string password, bool isAdmin, bool isMentor)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Phone = phone;
+            Adrress = adress;
+            Login = login;
+            Password = password;
+            IsAdmin = isAdmin;
+            IsMentor = isMentor;
+        }
+
     }
 }
